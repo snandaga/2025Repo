cec2f6fb1eb34f7602f274ff8c257eaadc8357bf
DELL <DELL@Sanjeev>
2025-09-29 17:02:01 +0530

    new

--- Maven_TestNG_Integration_Project/src/test/java/Com/JBK/MavenTest/LoginTest.java
+++ Maven_TestNG_Integration_Project/src/test/java/Com/JBK/MavenTest/LoginTest.java
@@ -2,6 +2,7 @@ package Com.JBK.MavenTest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -18,6 +19,13 @@ public class LoginTest {
 		
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless=new"); // For newer Chrome versions
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--remote-allow-origins=*");
+
+		WebDriver driver = new ChromeDriver(options);
 		driver.get("file:///F:/Automation%20Testing_Java%20By%20Kiran/Selenium%20Softwares/Selenium%20Softwares/Offline%20Website/Offline%20Website/index.html");
 		driver.manage().window().maximize();
 		
